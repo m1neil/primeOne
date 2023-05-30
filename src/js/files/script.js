c241d378ea509fbf34344c93932a9387e82f5ad9
Eric Alekseyev <ericalex0207@gmail.com>
2023-05-30 18:45:05 +0300

    done almost adaptive for header

---
+++ src/js/files/script.js
@@ -0,0 +1,38 @@
+const burger = document.querySelector('.icon-menu'),
+	menuMobile = document.querySelector('.header__menu-mobile');
+
+if (burger) {
+	burger.addEventListener('click', e => {
+		burger.classList.toggle('menu-open');
+		menuMobile.classList.toggle('menu-open');
+	});
+}
+
+// langeage
+
+headerAdaptiveMobile();
+
+window.addEventListener('resize', headerAdaptiveMobile);
+
+function headerAdaptiveMobile() {
+	const menuList = document.querySelectorAll('.header-bottom__menu'),
+		menuLanguage = document.querySelector('.header-top__lang'),
+		menuColumn = document.querySelectorAll('[data-header-menu]'),
+		width = window.innerWidth;
+	if (width < 768) {
+		menuList.forEach(menu => {
+			menuMobile.append(menu);
+		});
+		menuMobile.append(menuLanguage);
+	} else {
+		menuList.forEach(menu => {
+			if (menu.classList.contains('header-bottom__menu_fbjc-right')) {
+				menuColumn[1].append(menu);
+			} else {
+				menuColumn[0].append(menu);
+			}
+		});
+
+		document.querySelector('.header__top').prepend(menuLanguage);
+	}
+}
